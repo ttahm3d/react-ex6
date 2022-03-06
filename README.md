074dcee468627fd0bb2b838af9d35a41f3cd3964
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2022-03-06 21:12:12 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# react-ex6
-Created with CodeSandbox
+## ex06 the endgame (30m)
+
+### challenge
+
+Putting all the learning together. We would make a product page which has the functionalities working for these filters and sort.
+
+Final Solution: [https://nvi41.csb.app/](https://nvi41.csb.app/)
+
+Starting Point: [https://codesandbox.io/s/usereducer-infinity-wars-iwvvo](https://codesandbox.io/s/usereducer-infinity-wars-iwvvo)
+
+Note: Look for mdn docs for `sort()` functionality. And checkbox.
+
+Let's go step by step:
+
+1. Implement sorting by Price. Use Radio input element for this. **(10m)**
+
+   1. High to Low
+   2. Low to High
+
+2. Use checkbox to filter out of stock products **(5m)**
+
+3. Use checkbox to filter out products without fast delivery **(5m)**
+
+4. Use fieldset to cleanup the controls. **(5m)**
+
+5. Clean the overall implementation. Making sure every function is pure and putting things in different files. **(5m)**
+
+6. Homework: Implement the price range control.
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./styles.css";
+
+import faker from "faker";
+
+faker.seed(123);
+
+const data = [...Array(50)].map((item) => ({
+  id: faker.random.uuid(),
+  name: faker.commerce.productName(),
+  image: faker.random.image(),
+  price: faker.commerce.price(),
+  material: faker.commerce.productMaterial(),
+  brand: faker.lorem.word(),
+  inStock: faker.random.boolean(),
+  fastDelivery: faker.random.boolean(),
+  ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  offer: faker.random.arrayElement([
+    "Save 50",
+    "70% bonanza",
+    "Republic Day Sale"
+  ]),
+  idealFor: faker.random.arrayElement([
+    "Men",
+    "Women",
+    "Girl",
+    "Boy",
+    "Senior"
+  ]),
+  level: faker.random.arrayElement([
+    "beginner",
+    "amateur",
+    "intermediate",
+    "advanced",
+    "professional"
+  ]),
+  color: faker.commerce.color()
+}));
+
+export default function App() {
+  return (
+    <>
+      <div
+        className="App"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))"
+        }}
+      >
+        {data.map(
+          ({
+            id,
+            name,
+            image,
+            price,
+            productName,
+            inStock,
+            level,
+            fastDelivery
+          }) => (
+            <div
+              key={id}
+              style={{
+                border: "1px solid #4B5563",
+                borderRadius: "0 0 0.5rem 0.5rem",
+                margin: "1rem",
+                padding: "0 0 1rem"
+              }}
+            >
+              <img src={image} width="100%" height="auto" alt={productName} />
+              <h3> {name} </h3>
+              <div>Rs. {price}</div>
+              {inStock && <div> In Stock </div>}
+              {!inStock && <div> Out of Stock </div>}
+              <div>{level}</div>
+              {fastDelivery ? (
+                <div> Fast Delivery </div>
+              ) : (
+                <div> 3 days minimum </div>
+              )}
+            </div>
+          )
+        )}
+      </div>
+    </>
+  );
+}
